f21d6ab55f1910b91b82441a19172dfc625b31bb
andi <amitre@pegasus.rutgers.edu>
2014-02-17 19:29:55 -0500

    first commit

---
+++ Grade Calculator1/Grade Calculator/GradeCalculator.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GradeCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Mean = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Variance = New System.Windows.Forms.Button()
+        Me.Deviation = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label1.Location = New System.Drawing.Point(34, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Grade 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label2.Location = New System.Drawing.Point(34, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Grade 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label4.Location = New System.Drawing.Point(34, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Grade 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label5.Location = New System.Drawing.Point(34, 223)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Grade 5"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label3.Location = New System.Drawing.Point(34, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Grade 3"
+        '
+        'Mean
+        '
+        Me.Mean.BackColor = System.Drawing.Color.Transparent
+        Me.Mean.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Mean.ForeColor = System.Drawing.Color.SteelBlue
+        Me.Mean.Location = New System.Drawing.Point(255, 34)
+        Me.Mean.Name = "Mean"
+        Me.Mean.Size = New System.Drawing.Size(108, 30)
+        Me.Mean.TabIndex = 6
+        Me.Mean.Text = "Calculate Mean"
+        Me.Mean.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(102, 40)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(102, 84)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(102, 133)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(102, 178)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(102, 223)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox5.TabIndex = 11
+        '
+        'Variance
+        '
+        Me.Variance.BackColor = System.Drawing.Color.Transparent
+        Me.Variance.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Variance.ForeColor = System.Drawing.Color.SteelBlue
+        Me.Variance.Location = New System.Drawing.Point(392, 34)
+        Me.Variance.Name = "Variance"
+        Me.Variance.Size = New System.Drawing.Size(147, 30)
+        Me.Variance.TabIndex = 12
+        Me.Variance.Text = "Calculate Variance"
+        Me.Variance.UseVisualStyleBackColor = False
+        '
+        'Deviation
+        '
+        Me.Deviation.BackColor = System.Drawing.Color.Transparent
+        Me.Deviation.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Deviation.ForeColor = System.Drawing.Color.SteelBlue
+        Me.Deviation.Location = New System.Drawing.Point(574, 34)
+        Me.Deviation.Name = "Deviation"
+        Me.Deviation.Size = New System.Drawing.Size(186, 30)
+        Me.Deviation.TabIndex = 13
+        Me.Deviation.Text = "Calculate Standart Deviation"
+        Me.Deviation.UseVisualStyleBackColor = False
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(255, 84)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(108, 20)
+        Me.TextBox7.TabIndex = 14
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(392, 84)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(147, 20)
+        Me.TextBox8.TabIndex = 15
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(574, 84)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox9.TabIndex = 16
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label6.Location = New System.Drawing.Point(34, 267)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 16)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Grade 6"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(102, 267)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox6.TabIndex = 18
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(255, 205)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox10.TabIndex = 20
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Miramonte", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label7.Location = New System.Drawing.Point(252, 179)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(120, 16)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Letter Grade Earned"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(833, 309)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Deviation)
+        Me.Controls.Add(Me.Variance)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Mean)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Name = "Form1"
+        Me.Text = "Grade Calculator"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Mean As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Variance As System.Windows.Forms.Button
+    Friend WithEvents Deviation As System.Windows.Forms.Button
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ Grade Calculator1/Grade Calculator/GradeCalculator.vb
@@ -0,0 +1,146 @@
+﻿Public Class GradeCalculator
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Function findMean() As Double
+        Dim Int1, Int2, Int3, Int4, Int5, Int6, holdMean As Double
+
+
+        Try
+            Int1 = TextBox1.Text
+            Int2 = TextBox2.Text
+            Int3 = TextBox3.Text
+            Int4 = TextBox4.Text
+            Int5 = TextBox5.Text
+            Int6 = TextBox6.Text
+
+        Catch valueAsString As InvalidCastException
+            MessageBox.Show("Please make sure all the grades entered are numbers" & "        " & valueAsString.ToString)
+            Me.Close()
+        End Try
+
+
+        Dim numArray(5) As Double
+
+        numArray(0) = Int1
+        numArray(1) = Int2
+        numArray(2) = Int3
+        numArray(3) = Int4
+        numArray(4) = Int5
+        numArray(5) = Int6
+
+        holdMean = (numArray(0) + numArray(1) + numArray(2) + numArray(3) + numArray(4) + numArray(5)) / 6
+        Return holdMean
+
+    End Function
+
+    Public Sub Mean_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Mean.Click
+        Dim holdMean As Double
+        Dim stringMean As String
+        holdMean = (findMean())
+        stringMean = holdMean.ToString()
+        'Utilizing the substring method to only display the first five characters
+        TextBox7.Text = stringMean.Substring(0, 5)
+
+
+        'Assuming that the professor will round up or round down the grade just as in ceiling or floor
+        If holdMean >= 93.5 Then
+            TextBox10.Text = "A"
+        ElseIf holdMean >= 87.5 And holdMean <= 93.4 Then
+            TextBox10.Text = "B+"
+        ElseIf holdMean >= 81.5 And holdMean <= 87.4 Then
+            TextBox10.Text = "B"
+        ElseIf holdMean >= 75.5 And holdMean <= 81.4 Then
+            TextBox10.Text = "C+"
+        ElseIf holdMean >= 69.5 And holdMean <= 75.4 Then
+            TextBox10.Text = "C"
+        ElseIf holdMean >= 63.5 And holdMean <= 69.4 Then
+            TextBox10.Text = "D"
+        ElseIf holdMean <= 63.4 Then
+            TextBox10.Text = "F"
+        End If
+
+    End Sub
+
+    Private Function findVariance() As Double
+        Dim Int1, Int2, Int3, Int4, Int5, Int6 As Double
+        Int1 = TextBox1.Text
+        Int2 = TextBox2.Text
+        Int3 = TextBox3.Text
+        Int4 = TextBox4.Text
+        Int5 = TextBox5.Text
+        Int6 = TextBox6.Text
+
+        Dim numArray(5) As Double
+
+        numArray(0) = Int1
+        numArray(1) = Int2
+        numArray(2) = Int3
+        numArray(3) = Int4
+        numArray(4) = Int5
+        numArray(5) = Int6
+
+        Dim average As Double = (numArray(0) + numArray(1) + numArray(2) + numArray(3) + numArray(4) + numArray(5)) / 6
+
+        Return ((numArray(0) - average) ^ 2 + (numArray(1) - average) ^ 2 + (numArray(2) - average) ^ 2 + (numArray(3) - average) ^ 2 + (numArray(4) - average) ^ 2 + (numArray(5) - average) ^ 2)
+
+    End Function
+
+    Private Sub Variance_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Variance.Click
+        Dim holdVariance As Double
+        Dim answer As String
+        holdVariance = (findVariance())
+        answer = holdVariance.ToString()
+        'Used substring to only display the first 5 characters of the value
+        TextBox8.Text = answer.Substring(0, 5)
+
+    End Sub
+
+    Private Function findStdVariation() As Double
+        Dim stdAverage, stdAverageSquared As Double
+        Dim Int1, Int2, Int3, Int4, Int5, Int6 As Double
+        Dim newValue1, newValue2, newValue3, newValue4, newValue5, newValue6 As Double
+
+        Int1 = TextBox1.Text
+        Int2 = TextBox2.Text
+        Int3 = TextBox3.Text
+        Int4 = TextBox4.Text
+        Int5 = TextBox5.Text
+        Int6 = TextBox6.Text
+
+        Dim numArray(5) As Double
+
+        numArray(0) = Int1
+        numArray(1) = Int2
+        numArray(2) = Int3
+        numArray(3) = Int4
+        numArray(4) = Int5
+        numArray(5) = Int6
+
+        stdAverage = (numArray(0) + numArray(1) + numArray(2) + numArray(3) + numArray(4) + numArray(5)) / 6
+        newValue1 = (numArray(0) - stdAverage) ^ 2
+        newValue2 = (numArray(1) - stdAverage) ^ 2
+        newValue3 = (numArray(2) - stdAverage) ^ 2
+        newValue4 = (numArray(3) - stdAverage) ^ 2
+        newValue5 = (numArray(4) - stdAverage) ^ 2
+        newValue6 = (numArray(5) - stdAverage) ^ 2
+
+        stdAverageSquared = ((newValue1 + newValue2 + newValue3 + newValue4 + newValue5 + newValue6) / 6) ^ (1 / 2)
+
+        Return stdAverageSquared
+
+    End Function
+
+
+    Private Sub Deviation_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Deviation.Click
+        Dim holdStdVariation As Double
+        Dim variationValue As String
+        holdStdVariation = (findStdVariation())
+        'Converting value to string
+        variationValue = holdStdVariation.ToString()
+        'Utilizing the substring method to only display the first five characters of the value
+        TextBox9.Text = variationValue.Substring(0, 5)
+    End Sub
+End Class
